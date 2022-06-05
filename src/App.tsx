05ee75ead1c36ccacb90831bd32c535888aa3b22
Denis Isidoro <denis.isidoro@uber.com>
2022-06-05 19:36:14 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './bootstrap/App.css'
+import 'normalize.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+
+import React from 'react'
+
+function App() {
+   return (
+      <div className='App full-height'>
+         {"foo bar"}
+      </div>
+   )
+}
+
+export default App
